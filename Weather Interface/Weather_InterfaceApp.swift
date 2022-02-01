a529561d0f7e2a42084e454994700f18832aa758
Ali Butt <alibutt152209@gmail.com>
2022-02-01 13:40:48 +0500

    Initial Commit

---
+++ Weather Interface/Weather_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_InterfaceApp.swift
+//  Weather Interface
+//
+//  Created by Ali Butt on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
